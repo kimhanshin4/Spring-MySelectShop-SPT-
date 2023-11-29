2b73304bdd586e1c3c5391924a438181f0221d79
kimhanshin4 <funold1004@gmail.com>
2023-11-29 14:56:12 +0900

    test: Controller Test

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.*;
 import org.springframework.scheduling.annotation.*;
 
 @EnableScheduling
-@EnableJpaAuditing
 
 @SpringBootApplication
 public class MyselectshopApplication {
